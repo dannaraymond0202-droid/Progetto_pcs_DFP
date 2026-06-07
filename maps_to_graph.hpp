34209139b4d48234c83f920a9f7042ee5be15de3
Fedexair437 <finotti.fede@gmail.com>
2026-06-07 17:53:48 +0200

    Update maps_to_graph.hpp

--- maps_to_graph.hpp
+++ maps_to_graph.hpp
@@ -5,5 +5,14 @@ using namespace std;
 
 undirected_graph<int> circuit_graph_generator(map<int, array<double, 3>>& Res, map<int, array<double, 3>>& Vol){
   undirected_edge<int> G;
-  for(
+  for(auto& [n,info] : Res){
+    unidirected_edge<int> e(info.at(1), info.at(2));
+    G.add_edge(e);
+  }
+  for(auto& [n,info] : Vol){
+    unidirected_edge<int> e(info.at(1), info.at(2));
+    G.add_edge(e);
+  }
+
+  return G;
 }
