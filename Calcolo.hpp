0fad8d6d0604aead1b56da6106c180b7f3713cc4
Ray_02 <dannaraymond0202@gmail.com>
2026-06-07 19:24:13 +0200

    Refactor comment formatting in Calcolo.hpp

---
+++ Calcolo.hpp
@@ -0,0 +1,8 @@
+#include <Eigen/Dense>
+Eigen::VectorXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
+    Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B e una matice di                                           incidenza allora A sarà sempre una matrice simmetrica e definita positiva.
+    i0 = gradiente_coniugato(A, v, i0, tol);
+    Eigen::VectorXd Vr = R*B*i0;
+
+    return Vr;
+}
