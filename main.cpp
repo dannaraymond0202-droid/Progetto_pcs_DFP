d5f5e6121ad1692731eadc4385a4f95d9dc8f183
Ray_02 <dannaraymond0202@gmail.com>
2026-06-07 17:25:30 +0200

    pippo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <ostream>
+#include <fstream>
+#include <map>
+#include <array>
+#include <set>
+
+
 using namespace std;
 //standard per il verso di percorrenza delle correnti: senso ORARIO
 
-map<int, array<double, 3>> Res, Vol;  //raymond
+int main(){
+//Lettura File
+map<int, array<double, 3>> Res, Vol;
+string filename = "circuito.txt";
+int r = 1;
+int v = 1;
+ifstream ifs(filename);
+if (ifs.is_open()){//comando dii check dell'apertura per evitare errori 
+  while(!ifs.eof()){//fino a quando non arriva alla fine fa ....
+    string pip;
+    array<double,3> valori;
+    ifs >> pip;
+    set<char> poi;
+    for(int i = 0; i<pip.size(); i++){
+      poi.insert(pip[i]);
+    }
+    if(poi.contains('R')){ //creazione delle due mappa Res e Vol
+      cout<<"Ho letto correttamente R";
+      poi.clear();
+      ifs >> valori[0];
+      ifs >> valori[1];
+      ifs >> valori[2];
+      Res[r] = valori;
+      r ++;
+    }
+    else if(poi.contains('V')){
+      cout<<"Ho letto correttamnete V"; 
+      poi.clear();
+      ifs >> valori[0];
+      ifs >> valori[1]; 
+      ifs >> valori[2];
+      Vol[v] = valori;
+      v++;
+    }
+  }
+}
+
+
 
 auto G = circuit_graph_generator(Res, Vol);
 
