a1aa81b534a3edb20a2252e71d3ace09763959c0
elettrapancotti <elettra.pancotti@gmail.com>
2026-06-08 17:02:32 +0200

    Update Calcolo.hpp

--- Calcolo.hpp
+++ Calcolo.hpp
@@ -1,13 +1,14 @@
 #include <Eigen/Dense>
 #include "gradiente_coniugato.hpp"
-Eigen::MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
-    Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B è una matice di incidenza                                         incidenza allora A sarà sempre una matrice simmetrica e definita positiva.
+Eigen::MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, double tol){
+    Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B è una matice di incidenza allora A sarà sempre una matrice simmetrica e definita positiva
+    Eigen::VectorXd i0 = Eigen::VectorXd::Zero(B.cols());
     Eigen::VectorXd i = gradiente_coniugato(A, v, i0, tol);
-    Eigen::VectorXd Vr = R*B*i0;
+    Eigen::VectorXd Vr = R*B*i;
     Eigen::MatrixXd Risultato;
-    for(int i = 0; i<Vr.size(); i++){
-        Risultato(i,0) = Vr[i];
-        Risultato(i,1) = Vr[i]/R(i,i);
+    for(int j = 0; j <Vr.size(); j++){
+        Risultato(j,0) = Vr[j];
+        Risultato(j,1) = Vr[j]/R(j,j);
     }
     
     return Risultato;
