5df84cb92d38d33d62fe00b41cbffc9d3f04f09e
Ray_02 <dannaraymond0202@gmail.com>
2026-06-08 16:15:40 +0200

    Update Calcolo.hpp

--- Calcolo.hpp
+++ Calcolo.hpp
@@ -1,5 +1,5 @@
 #include <Eigen/Dense>
-Eigen:MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
+Eigen::MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
     Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B è una matice di incidenza                                         incidenza allora A sarà sempre una matrice simmetrica e definita positiva.
     Eigen::VectorXd i = gradiente_coniugato(A, v, i0, tol);
     Eigen::VectorXd Vr = R*B*i0;
