1388772f78b70000bf128c87852ffd248189f082
Ray_02 <dannaraymond0202@gmail.com>
2026-06-08 16:16:39 +0200

    Include gradiente_coniugato.hpp in Calcolo.hpp

--- Calcolo.hpp
+++ Calcolo.hpp
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include "gradiente_coniugato.hpp"
 Eigen::MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
     Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B è una matice di incidenza                                         incidenza allora A sarà sempre una matrice simmetrica e definita positiva.
     Eigen::VectorXd i = gradiente_coniugato(A, v, i0, tol);
