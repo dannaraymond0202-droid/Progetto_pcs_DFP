eaf23f953d595cad0221e1196d64e50a3d26a923
elettrapancotti <elettra.pancotti@gmail.com>
2026-06-08 15:38:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -77,10 +77,10 @@ int main(){
     Eigen::VectorXd I0 = Eigen::Vector::Zero(maglie.size);
     Eigen::VectorXd Ir = Eigen::Vector::Zero(Res.size);
     const double tol =  1.0e-15;
-    Eigen::VectorXd Vr = Calcolo(B,R,v,I0,tol);
+    Eigen::VectorXd Risultato = Calcolo(B,R,v,I0,tol); //matrice in cui nella prima colonna ci sono le Vr e nella seconda le Ir (numero di righe = numero di resistenze)
 
     for(auto& [n, info] : Res){
-        cout << "R" << n << ": V = " << Vr[n] << " volts, I = " << I0[n] << " amps.\n";
+        cout << "R" << n << ": V = " << Risultato(n-1,0) << " volts, I = " << Risultato(n-1,1) << " amps.\n";
     }
     
     return 0;
