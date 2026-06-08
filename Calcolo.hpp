26758965afb5436e7dfbeddafb2aad3570f33f6f
Ray_02 <dannaraymond0202@gmail.com>
2026-06-08 15:27:51 +0200

    Change return type of Calcolo to Eigen::MatrixXd
    Refactor Calcolo function to return Eigen::MatrixXd instead of Eigen::VectorXd. Update variable names and fix indexing in the result matrix.

--- Calcolo.hpp
+++ Calcolo.hpp
@@ -1,8 +1,13 @@
 #include <Eigen/Dense>
-Eigen::VectorXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
+Eigen:MatrixXd Calcolo(Eigen::MatrixXd& B, Eigen::MatrixXd& R , Eigen::VectorXd& v, Eigen::VectorXd& i0, double tol){
     Eigen::MatrixXd A = B.transpose()*R*B; //dato che la diagonale di R è sempre positiva e B è una matice di incidenza                                         incidenza allora A sarà sempre una matrice simmetrica e definita positiva.
-    i0 = gradiente_coniugato(A, v, i0, tol);
+    Eigen::VectorXd i = gradiente_coniugato(A, v, i0, tol);
     Eigen::VectorXd Vr = R*B*i0;
-
+    Eigen::MatrixXd Risultato;
+    for(int i = 0; i<Vr.size(); i++){
+        Risultato(i,1) = Vr[i];
+        Risultato(i,2) = Vr[i]/R[i][i];
+    }
+    
     return Vr;
 }
