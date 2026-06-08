48b75cb8d3db95e2698c47d8f0b4bf69b78debfc
Fedexair437 <finotti.fede@gmail.com>
2026-06-08 16:21:35 +0200

    Update maps_to_graph.hpp

--- maps_to_graph.hpp
+++ maps_to_graph.hpp
@@ -1,10 +1,11 @@
+#pragma once
 #include "unidirected_edges.hpp"
 #include "unidirected_graph.hpp"
 
 using namespace std;
 
 unidirected_graph<int> circuit_graph_generator(map<int, array<double, 3>>& Res, map<int, array<double, 3>>& Vol){
-  unidirected_edge<int> G;
+  unidirected_graph<int> G;
   for(auto& [n,info] : Res){
     G.add_edge(info.at(1), info.at(2));
   }
