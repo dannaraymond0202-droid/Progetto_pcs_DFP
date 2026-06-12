da85cb5348b5ddaedcd927dd713a1b78c920a0b1
Ray_02 <dannaraymond0202@gmail.com>
2026-06-12 16:36:05 +0200

    Update gradiente_coniugato.hpp

--- gradiente_coniugato.hpp
+++ gradiente_coniugato.hpp
@@ -1,19 +1,18 @@
 #include <Eigen/Dense>
-#include <Eigen/SVD>
 
 
 Eigen::VectorXd   gradiente_coniugato(Eigen::MatrixXd& A, Eigen::VectorXd& b, Eigen::VectorXd& x0,double tol){
-    Eigen::VectorXd r0 = b - A*x0;
+    Eigen::VectorXd r0 = b - A*x0; // 
     Eigen::VectorXd p0 = r0;
-    float n  = std::sqrt(A.size());  // faccio la radice perché il comado .size() ritorna 9 = 3*3 se n = 3;
+    float n  = std::sqrt(A.size());  // faccio la radice perché il comado .size() in una matrice ritorna 9 = 3*3 se n = 3;
     int k = 0;
-    Eigen::VectorXd x;
-    std::cout<<"Numero di iterazioni compiute: " ;
+    Eigen::VectorXd x; // definisco x come vettore soluzione
+    std::cout<<"Numero di iterazioni compiute dal metodo del gradiente: " ;
     while (k<10*n && r0.norm()>tol){
-        double alpha_k = ((p0.transpose()*r0) / (p0.transpose()*A *p0)).value();
+        double alpha_k = ((p0.transpose()*r0) / (p0.transpose()*A *p0)).value(); //definisco ad ogni iterazione alpha_k
         x = x0 + alpha_k*p0;
         r0 = b - A*x;
-        double betha_k = ((p0.transpose()*A*r0) / (p0.transpose()*A *p0)).value();
+        double betha_k = ((p0.transpose()*A*r0) / (p0.transpose()*A *p0)).value();  //definisco ad ogni iterazione  beta_k
         p0 = r0 - betha_k*p0;
         k = k+1;
         x0  = x;
@@ -21,11 +20,4 @@ Eigen::VectorXd   gradiente_coniugato(Eigen::MatrixXd& A, Eigen::VectorXd& b, Ei
     std::cout<< k<<"\n";
     return x;
 
-}
-
-
-double condA(const Eigen::MatrixXd& A){
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd(A);
-  Eigen::VectorXd singularValuesA = svd.singularValues();
-  return singularValuesA.maxCoeff() / singularValuesA.minCoeff();
 }
