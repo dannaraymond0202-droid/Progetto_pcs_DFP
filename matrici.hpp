f92351ce070d77f0ff90add136c5e5ea11e406e6
Fedexair437 <finotti.fede@gmail.com>
2026-06-12 14:35:04 +0200

    Update matrici.hpp

--- matrici.hpp
+++ matrici.hpp
@@ -26,17 +26,19 @@ void matrici(Eigen::MatrixXd& R, Eigen::VectorXd& v, Eigen::MatrixXd& B, const v
         i += 1;
     }
     
-    int j = 0;
-    for(auto& vet: maglie){ //vet è un vettore di archi
+    int j = 0;    //indice che tiene traccia delle maglie che consideriamo una per volta
+    //creiamo ora la matrice di incidenza B
+    for(auto& vet : maglie){ //vet è un vettore di archi
         for(auto& [key, obj]: Res){
-            unidirected_edge<int> a(obj[1], obj[2]);
-            for(auto& e : vet){ //ciclo su ogni arco del vettore della singola maglia
-                if(e == a && !(e.is_inv())){ //significa che stiamo attraversando la maglia nello stesso vero del resistore
+            unidirected_edge<int> b(obj[1], obj[2]);    //ora trattandosi di resistori il verso per convenzione non dipende più dall'ordine dei nodi nella netlist ma dal nodo minore al maggiore
+            for(auto& e : vet){ //ciclo su ogni arco del vettore corrispondente alla singola maglia
+                if(e == b && !(e.is_inv())){ //significa che stiamo attraversando la maglia nello stesso verso del resistore
                     B(key-1, j) = 1;
                 }
-                if(e == a && e.is_inv()){ //significa che stiamo attraversando la maglia nel senso opposto rispetto al resistore
+                if(e == b && e.is_inv()){ //significa che stiamo attraversando la maglia nel senso opposto rispetto al resistore
                     B(key-1, j) = -1;
                 } 
+                //se la resistenza "key" non appartiene alla maglia j lasciamo 0 nella posizione corrispondente (jey-1, j) della matrice B
             }
         }
         j += 1;
