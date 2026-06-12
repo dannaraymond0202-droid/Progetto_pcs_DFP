d43a3313e80dd55ae2ef4fd1b44532e099072a42
Ray_02 <dannaraymond0202@gmail.com>
2026-06-12 12:01:09 +0200

    Enhance comments in circuit_graph_generator function
    Added comments to clarify graph generation process.

--- maps_to_graph.hpp
+++ maps_to_graph.hpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 unidirected_graph<int> circuit_graph_generator(map<int, array<double, 3>>& Res, map<int, array<double, 3>>& Vol){
-  unidirected_graph<int> G;
+  unidirected_graph<int> G;  //dichiarazione del grafo 
   for(auto& [n,info] : Res){
-    G.add_edge(info.at(1), info.at(2));
+    G.add_edge(info.at(1), info.at(2)); //inizializzazione chiamando add_edge()
   }
   for(auto& [n,info] : Vol){
     G.add_edge(info.at(1), info.at(2));
   }
-
+       //in Unidirect_edges.hpp abbiamo agigutno un menbro privato booleano per verificare la direzione del componente  
   return G;
 }
